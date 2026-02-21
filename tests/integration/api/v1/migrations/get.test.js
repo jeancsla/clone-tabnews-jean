ccb6200bf222d38e0c99bf33d80ba0965ae2e2e4
Jean Aniceto <jeancsla@gmail.com>
2026-02-21 14:10:56 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,14 +7,18 @@ beforeAll(async () => {
   await database.query("CREATE SCHEMA public;");
 });
 
-test("GET /api/v1/migrations deve retornar status 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  // console.log(responseBody);
+      const responseBody = await response.json();
+      // console.log(responseBody);
 
-  expect(Array.isArray(responseBody)).toBe(true);
+      expect(Array.isArray(responseBody)).toBe(true);
 
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
