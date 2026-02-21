1cae687acd9e06e84a46063d53645c6db41b7838
Jean Aniceto <jeancsla@gmail.com>
2026-02-21 14:10:56 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,12 +7,11 @@ beforeAll(async () => {
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    test("Running pending migrations", async () => {
+    test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
 
       const responseBody = await response.json();
-      // console.log(responseBody);
 
       expect(Array.isArray(responseBody)).toBe(true);
 
