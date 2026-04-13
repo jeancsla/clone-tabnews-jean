5da121a7ec0ce66ac03bbe07f7a2851746982fa7
Jean Aniceto <jeancsla@gmail.com>
2026-04-12 22:51:12 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

---
+++ infra/controller.js
@@ -0,0 +1,25 @@
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    statusCode: error.statusCode,
+    cause: error,
+  });
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+
+  console.error(publicErrorObject);
+}
+
+const controller = {
+  errorHandlers: {
+    onNoMatch: onNoMatchHandler,
+    onError: onErrorHandler,
+  },
+};
+
+export default controller;
